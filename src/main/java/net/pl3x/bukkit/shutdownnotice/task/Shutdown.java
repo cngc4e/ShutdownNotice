99b5997a7c55e4a16cd85ec4c1da79b15cb82dfa
Billy Galbreath <BillyGalbreath@Hotmail.com>
2015-06-20 13:26:14 -0500

    v2.8

--- src/main/java/net/pl3x/bukkit/shutdownnotice/task/Shutdown.java
+++ src/main/java/net/pl3x/bukkit/shutdownnotice/task/Shutdown.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import net.pl3x.bukkit.shutdownnotice.Logger;
 import net.pl3x.bukkit.shutdownnotice.Main;
 import net.pl3x.bukkit.shutdownnotice.ServerStatus;
 import net.pl3x.bukkit.shutdownnotice.ServerStatus.State;
@@ -35,6 +36,7 @@ public class Shutdown extends BukkitRunnable {
 
 		if (state.equals(State.RESTART)) {
 			// create blank restart file
+			Logger.debug("Creating restart file.");
 			try {
 				new File(plugin.getDataFolder(), "restart").createNewFile();
 			} catch (IOException e) {
@@ -48,7 +50,9 @@ public class Shutdown extends BukkitRunnable {
 		// run configured commands before shutting down
 		List<String> shutdownCommands = Config.SHUTDOWN_COMMANDS.getStringList();
 		for (String command : shutdownCommands) {
-			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command.replace("{action}", action).replace("{reason}", reason));
+			command = command.replace("{action}", action).replace("{reason}", reason);
+			Logger.debug("Performing command: " + command);
+			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
 		}
 
 		// always perform save-all command
@@ -56,12 +60,13 @@ public class Shutdown extends BukkitRunnable {
 
 		// always kick all players
 		String kickMessage = ChatColor.translateAlternateColorCodes('&', Lang.KICK_MESSAGE.get().replace("{action}", action).replace("{reason}", reason));
-
 		for (Player player : Bukkit.getOnlinePlayers()) {
+			Logger.debug("Kicking player: " + player.getName() + "(" + kickMessage + ")");
 			player.kickPlayer(kickMessage);
 		}
 
 		// finally stop the server
+		Logger.debug("Performing command: stop");
 		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "stop");
 	}
 }
